ead66c4321c493fd0ad92536da7c0dea40eb71f9
Stanislav Zhuk <stasadev@gmail.com>
2025-10-30 00:04:04 +0200

    chore(*): prepare for official release (#7)

--- homeadditions/.bashrc.d/pnpm.sh
+++
@@ -1,8 +0,0 @@
-#ddev-generated
-
-# Add pnpm home location to the path.
-export PNPM_HOME="$HOME/.local/share/pnpm"
-case ":$PATH:" in
-  *":$PNPM_HOME:"*) ;;
-  *) export PATH="$PNPM_HOME:$PATH" ;;
-esac
